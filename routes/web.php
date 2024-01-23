b6aa05ae82f4263e083488fe5e295fd0ad83e3a3
ryokf <ryokhrisnaf@gmail.com>
2024-01-23 16:21:58 +0700

    ✨feature: setup controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\CustomerController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\IncomeController;
+use App\Http\Controllers\Admin\IngredientController;
+use App\Http\Controllers\Admin\OutcomeController;
+use App\Http\Controllers\Admin\SettingController;
+use App\Http\Controllers\Admin\StoreController;
+use App\Models\Income;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -15,5 +23,52 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    return 'halo';
 });
+
+Route::get('/admin/dashboard',[DashboardController::class,'index'])->name('dashboard');
+
+Route::controller(DashboardController::class)->group(function () {
+    Route::get('/admin', 'index');
+});
+
+Route::controller(OutcomeController::class)->group(function () {
+    Route::get('/admin/outcome', 'index');
+    Route::post('/admin/outcome', 'store');
+    Route::put('/admin/outcome/', 'update');
+    Route::delete('/admin/outcome/', 'destroy');
+});
+
+Route::controller(IncomeController::class)->group(function () {
+    Route::get('/admin/income', 'index');
+    Route::post('/admin/income', 'store');
+    Route::put('/admin/income/', 'update');
+    Route::delete('/admin/income/', 'destroy');
+});
+
+Route::controller(StoreController::class)->group(function () {
+    Route::get('/admin/store', 'index');
+    Route::post('/admin/store', 'store');
+    Route::put('/admin/store/', 'update');
+    Route::delete('/admin/store/', 'destroy');
+});
+
+Route::controller(IngredientController::class)->group(function () {
+    Route::get('/admin/ingredient', 'index');
+    Route::post('/admin/ingredient', 'store');
+    Route::put('/admin/ingredient/', 'update');
+    Route::delete('/admin/ingredient/', 'destroy');
+});
+
+Route::controller(CustomerController::class)->group(function () {
+    Route::get('/admin/customer', 'index');
+    Route::post('/admin/customer', 'store');
+    Route::put('/admin/customer/', 'update');
+    Route::delete('/admin/customer/', 'destroy');
+});
+
+Route::controller(SettingController::class)->group(function () {
+    Route::get('/admin/setting', 'index');
+});
+
+
