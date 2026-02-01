32d59dea39550997fe76ecfce64312f59d4d38d9
ryokf <ryokhrisnaf@gmail.com>
2026-02-01 22:51:55 +0700

    setup api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { ProductController } from './controller/product/product.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ProductModule],
+  controllers: [AppController, ProductController],
   providers: [AppService],
 })
 export class AppModule {}
