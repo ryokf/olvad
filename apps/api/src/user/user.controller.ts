9a2e1c006c05c5075b00d20581b18c9f3d8d7845
ryokf <ryokhrisnaf@gmail.com>
2026-02-03 23:28:00 +0700

    ✨feature:

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,26 +1,18 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
+import { UserService } from './user.service';
 
 @Controller('/api')
 export class UserController {
-    constructor(private readonly prisma: PrismaService) {}
+    constructor(private readonly userService: UserService) {}
 
     @Post('/register')
     async register(
         @Body('data')
         data: RegisterDto,
     ): Promise<string> {
-        await this.prisma.user.create({
-            data: {
-                username: data.username,
-                email: data.email,
-                photo: data.photo,
-                address: data.address,
-                password: data.password,
-            },
-        });
+        await this.userService.register(data);
 
-        return 'Hello Users';
+        return 'User registered successfully';
     }
 }
--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../generated/prisma/client';
+import { RegisterDto } from './dto/register.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async register(data: RegisterDto): Promise<User> {
+        const user = await this.prisma.user.create({
+            data: {
+                username: data.username,
+                email: data.email,
+                photo: data.photo,
+                address: data.address,
+                password: data.password,
+            },
+        });
+
+        return user;
+    }
+}
