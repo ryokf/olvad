4007ba038f5e97f35bda630be0574c5721c04286
ryokf <ryokhrisnaf@gmail.com>
2026-02-05 23:04:35 +0700

    ✨feature: implement ProductService with CRUD operations and corresponding tests

--- apps/api/src/product/product.controller.ts
+++ apps/api/src/product/product.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import type { Product } from '../generated/prisma/client';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(private readonly productService: ProductService) {}
+
+    @Get('/')
+    async getAllProducts(): Promise<Array<Product>> {
+        const products = await this.productService.getAllProduct();
+
+        return products;
+    }
+
+    @Get('/:id')
+    async getProductById(@Param('id') id: string): Promise<Product | null> {
+        const product = await this.productService.getProductById(Number(id));
+
+        return product;
+    }
+
+    @Post('/')
+    async createProduct(@Body('data') data: Product): Promise<Product | null> {
+        const product = await this.productService.createProduct(data);
+
+        return product;
+    }
+
+    @Put('/:id')
+    async editProduct(
+        @Param('id') id: string,
+        @Body('data') data: Product,
+    ): Promise<Product | null> {
+        const product = await this.productService.editProduct(Number(id), data);
+
+        return product;
+    }
+
+    @Delete('/:id')
+    async deleteProduct(@Param('id') id: string): Promise<Product | null> {
+        const product = await this.productService.deleteProduct(Number(id));
+
+        return product;
+    }
+}
---
+++ apps/api/src/product/product.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from '../generated/prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class ProductService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async getAllProduct(): Promise<Array<Product>> {
+        const products = await this.prisma.product.findMany();
+
+        return products;
+    }
+
+    async getProductById(id: number): Promise<Product | null> {
+        const product = await this.prisma.product.findUnique({
+            where: {
+                id: id,
+            },
+        });
+
+        return product;
+    }
+
+    async createProduct(data: Product): Promise<Product> {
+        const product = this.prisma.product.create({
+            data: {
+                name: data.name,
+                description: data.description,
+                categoryId: data.categoryId,
+                photo: data.photo,
+                price: data.price,
+                tags: data.tags,
+            },
+        });
+
+        return product;
+    }
+
+    async editProduct(id: number, data: Product): Promise<Product> {
+        const product = this.prisma.product.update({
+            where: {
+                id,
+            },
+            data: {
+                name: data.name,
+                description: data.description,
+                categoryId: data.categoryId,
+                photo: data.photo,
+                price: data.price,
+                tags: data.tags,
+            },
+        });
+
+        return product;
+    }
+
+    async deleteProduct(id: number): Promise<Product> {
+        const product = await this.prisma.product.delete({
+            where: {
+                id: id,
+            },
+        });
+
+        return product;
+    }
+}
